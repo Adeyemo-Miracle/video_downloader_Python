a6a02a752ca49fa6e983a95e85bdd79dd77e78a8
Adeyemo-Miracle <114000181+Adeyemo-Miracle@users.noreply.github.com>
2025-12-07 12:05:02 -0800

    Inputs

--- downloader.py
+++ downloader.py
@@ -1,3 +1,12 @@
-# import requests
+import requests
 Url = input('Url: ')
+if not Url.startswith('http://') and not Url.startswith('https://'): 
+    print('Invalid URL. Please include http:// or https://')
+    exit(1)
 filename = input('Filename: ')
+response = requests.get(Url, stream=True)
+with open(filename, 'wb') as file:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            file.write(chunk)
+print(f'Download completed: {filename}')
