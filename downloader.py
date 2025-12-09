bbff9c2f714fafa96c4e694d0b8f5c35947ac08e
Adeyemo-Miracle <114000181+Adeyemo-Miracle@users.noreply.github.com>
2025-12-09 08:01:46 -0800

    Modified downloader.py to improve Url validation by ensuring it ends with .mp4 and also added Exception for  handling possible errors.

--- downloader.py
+++ downloader.py
@@ -1,17 +1,21 @@
 import requests
 while True:
     Url = input('Url: ')
-    if not Url.startswith('http://') and not Url.startswith('https://'): 
+    if not Url.startswith('http://', 'https://') and not Url.endswith('.mp4'): 
         print('Invalid URL. Please include http:// or https://')
     # print('Exiting...')
     # exit(1)
     else:
         print('Valid Url')
         break
-# filename = Video.mp4
-# response = requests.get(Url, stream=True)
-# with open(filename, 'wb') as file:
-#     for chunk in response.iter_content(chunk_size=1024):
-#         if chunk:
-#             file.write(chunk)
-# print(f'Download completed: {filename}')
+filename = 'Video.mp4'
+try:
+    response = requests.get(Url, stream=True)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                file.write(chunk)
+    print(f'Download completed: {filename}')
+except Exception as e:
+    print(f"A error occurred: {e}")
